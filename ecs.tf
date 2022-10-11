f87974e38dd77d88d8f70674d59f587b6a66cde4
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2022-10-11 09:48:44 -0700

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -2,21 +2,8 @@ resource "aws_ecs_task_definition" "service" {
   family = "service"
   container_definitions = jsonencode([
     {
-      name      = "first"
-      image     = "nginx"
-      cpu       = 10
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    },
-    {
       name      = "second"
-      image     = "python:3.9-alpine"
+      image     = "node:4.8.1-slim"
       cpu       = 10
       memory    = 256
       essential = true
